d1cd70afffccc304639a27db1734bb8b51ad6b5f
Freddy Caceres <freddy@freddygraphics.com>
2026-02-06 12:01:18 -0500

    force dynamic protected layout

--- app/(protected)/layout.jsx
+++ app/(protected)/layout.jsx
@@ -1,6 +1,7 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
-
 import MainShell from "@/components/layout/MainShell";
 
 export default async function ProtectedLayout({ children }) {
