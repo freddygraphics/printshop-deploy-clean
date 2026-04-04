352a980ef409c4236c7ae988cb5aacb8c999b5df
Freddy Caceres <freddy@freddygraphics.com>
2026-04-04 17:35:18 -0400

    Fix puppeteer chromium

--- app/api/invoices/[id]/pdf/route.js
+++ app/api/invoices/[id]/pdf/route.js
@@ -4,6 +4,7 @@ export const dynamic = "force-dynamic";
 import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
 
+// ⚡ CACHE
 const pdfCache = new Map();
 
 export async function GET(req, context) {
@@ -20,13 +21,25 @@ export async function GET(req, context) {
       });
     }
 
-    const browser = await puppeteer.launch({
-      headless: true,
-    });
+    const isDev = process.env.NODE_ENV !== "production";
+
+    const browser = await puppeteer.launch(
+      isDev
+        ? {
+            headless: true,
+            // 👇 ESTO ARREGLA LOCAL
+            channel: "chrome",
+          }
+        : {
+            args: chromium.args,
+            executablePath: await chromium.executablePath(),
+            headless: true,
+          },
+    );
 
     const page = await browser.newPage();
 
-    // 🔥 URL DINÁMICA (FIX IMPORTANTE)
+    // 🔥 URL DINÁMICA (IMPORTANTE)
     const url = new URL(req.url);
     const baseUrl = `${url.protocol}//${url.host}`;
 
